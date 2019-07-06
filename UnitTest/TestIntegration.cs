b0a6b59e978a4461550646f233f4c623162a8e6d
Jack Zhang <hong.jack.zhang@gmail.com>
2019-07-06 07:31:30 -0400

    added integration test

---
+++ UnitTest/TestIntegration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UnitTest
+{
+    [TestFixture(Category = "Integration")]
+    class TestIntegration
+    {
+        [Test]
+        public void TestIntg()
+        {
+            Assert.IsTrue(1 == 1);
+        }
+    }
+}
